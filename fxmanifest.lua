082acd75a387f113f26252b5473729e00a25e5fd
J0hnDeL0rean <lauriszproductions@gmail.com>
2021-09-17 12:38:13 +0300

    Loading screen base

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+-- Made by John DeLorean Studios https://delorean.lt/discord/
+-- I will provide all the necessary support for it :P
+
+fx_version "bodacious"
+game "gta5"
+
+loadscreen 'index.html'
+loadscreen_manual_shutdown "yes"
+
+files {
+    "index.html",
+	"css/main.css",
+	"music.mp3"
+}
+
+client_script 'client.lua'
